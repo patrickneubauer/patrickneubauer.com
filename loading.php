2c19a72da012e42af8e5629ed970e49561ee2b60
Patrick Neubauer <mail@patrickneubauer.com>
2025-04-18 10:27:40 +0200

    initial commit

---
+++ loading.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="UTF-8">
+  <title>patrick neubauer</title>
+
+  <link rel="stylesheet" type="text/css" href="https://patrickneubauer.com/css/loading-bar.min.css"/>
+  <script type="text/javascript" src="https://patrickneubauer.com/js/loading-bar.min.js"></script>
+
+  <style>
+    .loading {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  </style>
+
+</head>
+
+<body>
+
+<div class="ldBar loading" data-type="fill" data-img="img/icons/circled/rocket4.png"></div>
+
+</body>
+</html>
+
